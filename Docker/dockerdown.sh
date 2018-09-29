7c59bfa9e9fc312b0bb06e331803adfa07f68bac
oh eun Kwon <dhdms6512@gmail.com>
2018-09-30 01:46:00 +0900

    update

--- Docker/dockerdown.sh
+++ Docker/dockerdown.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
-echo "########RUNNING CONTAINERS########"
-containers=$(sudo docker ps -aq)
-if test -n "$containers"
-    then
-    echo ${containers}
-    echo "########STOPPING########"
-    sudo docker stop ${containers}
+if [$(id -u) -ne 0]
+then
+    echo "########RUNNING CONTAINERS########"
     containers=$(sudo docker ps -aq)
-    #if it dosen't completely shutted down
-    if test -n "$containers";
-    then
-    echo "########CONTAINER REMOVE INIT########"
-    sudo docker container rm ${containers}
-    containers=$(sudo docker ps -aq)
-        if test -z "$containers";
+    if test -n "$containers"
         then
+        echo ${containers}
+        echo "########STOPPING########"
+        sudo docker stop ${containers}
+        containers=$(sudo docker ps -aq)
+        #if it dosen't completely shutted down
+        if test -n "$containers";
+        then
+        echo "########CONTAINER REMOVE INIT########"
+        sudo docker container rm ${containers}
+        containers=$(sudo docker ps -aq)
+            if test -z "$containers";
+            then
+            echo "########COMPLETE########"
+            fi
+        else
         echo "########COMPLETE########"
         fi
-    else
-    echo "########COMPLETE########"
+        else
+        echo "!!!!!!!!NO CONTAINER!!!!!!!!"
     fi
-    else
-    echo "!!!!!!!!NO CONTAINER!!!!!!!!"
+else
+    echo "########RUNNING CONTAINERS########"
+        containers=$(docker ps -aq)
+        if test -n "$containers"
+            then
+            echo ${containers}
+            echo "########STOPPING########"
+            docker stop ${containers}
+            containers=$(docker ps -aq)
+            #if it dosen't completely shutted down
+            if test -n "$containers";
+            then
+            echo "########CONTAINER REMOVE INIT########"
+            docker container rm ${containers}
+            containers=$(docker ps -aq)
+                if test -z "$containers";
+                then
+                echo "########COMPLETE########"
+                fi
+            else
+            echo "########COMPLETE########"
+            fi
+            else
+            echo "!!!!!!!!NO CONTAINER!!!!!!!!"
+        fi
 fi
