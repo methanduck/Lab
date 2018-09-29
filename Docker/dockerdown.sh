60ff2a467500edda122025794c9777c9c9a3d378
oh eun Kwon <dhdms6512@gmail.com>
2018-09-30 01:40:37 +0900

    update

--- Docker/dockerdown.sh
+++ Docker/dockerdown.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 echo "########RUNNING CONTAINERS########"
 containers=$(sudo docker ps -aq)
-echo ${containers}
-echo "########STOPPING########"
-sudo docker stop ${containers}
-containers=$(sudo docker ps -aq)
-#if it dosen't completely shutted down
-if test -n "$containers";
-then
-echo "########CONTAINER REMOVE INIT########"
-sudo docker container rm ${containers}
-containers=$(sudo docker ps -aq)
-    if test -z "$containers";
+if test -n "$containers"
     then
+    echo ${containers}
+    echo "########STOPPING########"
+    sudo docker stop ${containers}
+    containers=$(sudo docker ps -aq)
+    #if it dosen't completely shutted down
+    if test -n "$containers";
+    then
+    echo "########CONTAINER REMOVE INIT########"
+    sudo docker container rm ${containers}
+    containers=$(sudo docker ps -aq)
+        if test -z "$containers";
+        then
+        echo "########COMPLETE########"
+        fi
+    else
     echo "########COMPLETE########"
     fi
-else
-echo "########COMPLETE########"
 fi
