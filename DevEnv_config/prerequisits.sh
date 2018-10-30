ea7b97f63231f425b353779e75d31340a61c2f0d
OhEun Kwon <dhdms6512@gmail.com>
2018-10-30 23:08:44 +0900

    Update prerequisits.sh

--- DevEnv_config/prerequisits.sh
+++ DevEnv_config/prerequisits.sh
@@ -27,7 +27,10 @@ CHKER=$(dpkg -l | grep golang-1.9-go)
         echo "########Go Lang Env setting init########"    
         echo -e "GOPATH (ex: /home/kwon/go) : \c"
         read path
+        echo "########exporting GOPATH########"
+        export GOPATH=$path
         echo "export GOPATH=$path" >> $HOME/.bashrc
+        export PATH=/usr/lib/go-1.9/bin:$GOPATH/bin:$PATH
         echo "export PATH=/usr/lib/go-1.9/bin:$GOPATH/bin:$PATH" >> $HOME/.bashrc
     fi
 echo "########INSTALL : PYTHON########"
