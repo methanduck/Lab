387c823415c81f670c41256a7487aa411baf713d
root <dhdms6512@gmail.com>
2018-11-02 14:31:21 +0900

    update

--- DevEnv_config/CPU_onff.sh
+++ DevEnv_config/CPU_onff.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
-if [ $(id -u) - ne 0 ]
+if [ $(id -u) -ne 0 ]
 then 
     echo "!!!!!!!!please run as root!!!!!!!!"
 else
-    for core in {$1...$2}
+   if [ $# -lt 4 ] 
+    then
+    count=$1
+    while [ $count -le $2 ]  
     do
-     echo $3 | sudo tee /sys/devices/system/cpu/cpu$core/online
+     echo $3 | sudo tee /sys/devices/system/cpu/cpu$count/online
+     echo "CPU CORE $COUNT STATUS : $(sudo cat /sys/devices/system/cpu/cpu$count/online)"
+     count=$((count + 1))
      done
+   else
+     echo "Require 3 Args [start core Num] [End core Num] [Active:1 Deactive:0]"
+   fi
 fi     
